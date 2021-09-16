002412f352ff2d1a5607e3c8a015f8459a1525aa
charade6 <charade6@naver.com>
2021-09-16 19:39:16 +0900

    Edit App.js
    map함수 이용하여 Food컴포넌트 여러개 만들기

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-
+const foodILike = [
+  {
+    name: "chicken",
+    image: "https://health.chosun.com/site/data/img_dir/2021/03/31/2021033102448_0.jpg"
+  },
+  {
+    name: "pizza",
+    image: "https://src.hidoc.co.kr/image/lib/2020/11/9/1604911318873_0.jpg"
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
+      {foodILike.map(dish => (
+        <Food name={dish.name} />
+      ))}
     </div>
   );
 }
 
-function Food({fav}) {
-  return <h1>I like {fav}</h1>
+function Food({name}) {
+  return (
+    <h1>I like {name}</h1>
+  )
 }
 
 export default App;
