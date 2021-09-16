e54f32da53f0cfe31bbcdf43a70dd3ae8166c43a
charade6 <charade6@naver.com>
2021-09-16 19:12:43 +0900

    Edit App.js
    props 사용하여 콘솔출력

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-function Potato() {
-  return <h1>I like potato</h1>
-}
+
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      <Food fav="kimchi" something={true} papapa={['hello', 1, 2, true]} />
     </div>
   );
+}
+
+function Food(foo) {
+  console.log(foo)
+  return <h1>I like potato</h1>
 }
 
 export default App;
