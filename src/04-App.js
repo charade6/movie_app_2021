8ce3e63e9cc7724746c06e70ffc5cdc3ad2cd149
charade6 <charade6@naver.com>
2021-10-02 17:30:42 +0900

    Edit App.js
    클래스형 컴포넌트로 App.js작성

---
+++ src/04-App.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types'
+
+const foodILike = [
+  {
+    id: 1,
+    name: "chicken",
+    image: "https://health.chosun.com/site/data/img_dir/2021/03/31/2021033102448_0.jpg",
+    alt: "치킨",
+    rating: 4.5
+  },
+  {
+    id: 2,
+    name: "pizza",
+    image: "https://src.hidoc.co.kr/image/lib/2020/11/9/1604911318873_0.jpg",
+    alt: "피자",
+    rating: 4.3
+  }
+]
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+}
+
+function App() {
+  return (
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} alt={dish.alt} rating={dish.rating} />
+      ))}
+    </div>
+  );
+}
+
+function Food({name, picture, alt, rating}) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={alt}/>
+    </div>
+  )
+}
+
+export default App
--- src/App.js
+++ src/App.js
@@ -1,46 +1,11 @@
-import PropTypes from 'prop-types'
+import React from 'react'
 
-const foodILike = [
-  {
-    id: 1,
-    name: "chicken",
-    image: "https://health.chosun.com/site/data/img_dir/2021/03/31/2021033102448_0.jpg",
-    alt: "치킨",
-    rating: 4.5
-  },
-  {
-    id: 2,
-    name: "pizza",
-    image: "https://src.hidoc.co.kr/image/lib/2020/11/9/1604911318873_0.jpg",
-    alt: "피자",
-    rating: 4.3
-  }
-]
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} alt={dish.alt} rating={dish.rating} />
-      ))}
-    </div>
-  );
-}
-
-function Food({name, picture, alt, rating}) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={alt}/>
-    </div>
-  )
+class App extends React.Component {
+    render() {
+        return (
+            <h1>Hello</h1>
+        )
+    }
 }
 
 export default App
