34d2a4e0a727dd715e2243b20c849c717e3d93c7
charade6 <charade6@naver.com>
2021-10-02 16:53:53 +0900

    Edit App.js
    proptypes사용

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
+import PropTypes from 'prop-types'
+
 const foodILike = [
   {
     id: 1,
     name: "chicken",
-    image: "https://health.chosun.com/site/data/img_dir/2021/03/31/2021033102448_0.jpg"
+    image: "https://health.chosun.com/site/data/img_dir/2021/03/31/2021033102448_0.jpg",
+    alt: "치킨",
+    rating: 4.5
   },
   {
     id: 2,
     name: "pizza",
-    image: "https://src.hidoc.co.kr/image/lib/2020/11/9/1604911318873_0.jpg"
+    image: "https://src.hidoc.co.kr/image/lib/2020/11/9/1604911318873_0.jpg",
+    alt: "피자",
+    rating: 4.3
   }
 ]
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.string.isRequired
+}
 
 function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} alt={dish.alt} rating={dish.rating} />
       ))}
     </div>
   );
 }
 
-function Food({name, picture}) {
+function Food({name, picture, alt, rating}) {
   return (
     <div>
       <h1>I like {name}</h1>
-      <img src={picture} alt={name}/>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={alt}/>
     </div>
   )
 }
