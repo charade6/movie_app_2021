bd045c2c23a0312e32687ec737178b5784e70e22
charade6 <charade6@naver.com>
2021-10-02 19:26:56 +0900

    Edit App.js
    count state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import {Component} from 'react'
 
-class App extends React.Component {
+class App extends Component {
+    state = {
+        count: 0
+    }
+
+    add = () => {
+        this.setState({count: this.state.count + 1})
+    }
+
+    minus = () => {
+        this.setState({count: this.state.count - 1})
+    }
+
     render() {
         return (
-            <h1>Hello</h1>
+            <div>
+                <h1>The number is: {this.state.count} </h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
         )
     }
 }
