1862146e5eb30d606e435208d9c7e74bf9790650
charade6 <charade6@naver.com>
2021-10-09 20:51:30 +0900

    Edit App.js
    영화데이터 저장

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Component } from "react";
+import { Component } from "react"
+import axios from "axios"
 
 class App extends Component {
     state = {
@@ -6,16 +7,22 @@ class App extends Component {
         movies: []
     }
 
+    getMoives = async () => {
+        const {
+            data: {
+                data: {movies}
+            }
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json')
+        this.setState({movies, isLoading: false})
+    }
     componentDidMount(){
-        setTimeout(() => {
-            this.setState({isLoading: false})
-        }, 6000);
+        this.getMoives()
     }
     render(){
         const {isLoading} = this.state
         return(
             <div>
-                {isLoading ? <img src="img/loading.gif" alt="loading"/> : '영화 데이터 출력'}
+                {isLoading ? <img src="img/loading.gif" width="100px" height="100px" alt="loading"/> : '영화 데이터 출력'}
             </div>
         )
     }
