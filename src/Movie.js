1affd32388bf26602f820c74524037a1fd873138
charade6 <charade6@naver.com>
2021-10-16 06:46:02 +0900

    Edit Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,18 @@
 import Proptypes from 'prop-types'
+import "./Movie.css"
 
-function Movie({title, year, summary, poster}) {
+function Movie({title, year, summary, poster, genres}) {
     return (
-        <div class="movie-data">
+        <div className="movie-data">
             <img src={poster} alt={title} title={title} />
-            <h3 class="movie-title">{title}</h3>
-            <h5 class="movie-year">{year}</h5>
-            <p class="movie-summary">{summary}</p>
+            <h3 className="movie-title">{title}</h3>
+            <h5 className="movie-year">{year}</h5>
+            <ul className="movie-genres">
+                {genres.map((genre) => {
+                    return <li className="movie-genre">{genre}</li>
+                })}
+            </ul>
+            <p className="movie-summary">{summary}</p>
         </div>
     )
 }
@@ -15,7 +21,8 @@ Movie.propTypes = {
     year: Proptypes.number.isRequired,
     title: Proptypes.string.isRequired,
     summary: Proptypes.string.isRequired,
-    poster: Proptypes.string.isRequired
+    poster: Proptypes.string.isRequired,
+    genres: Proptypes.arrayOf(Proptypes.string).isRequired
 }
 
 export default Movie
