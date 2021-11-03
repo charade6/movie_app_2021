4c5f3dc878d77becad7f86dd76a991fe0919ca7e
charade6 <charade6@naver.com>
2021-11-03 16:38:19 +0900

    Edit Detail.js
    클래스형 컴포넌트 변경
    push함수

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,19 @@
-function Detail(props) {
-    console.log(props)
-    return (
-        <span>hello</span>
-    )
+import { Component } from "react"
+
+class Detail extends Component {
+    componentDidMount(){
+        const {location, history} = this.props
+        if(location.state === undefined){
+            history.push("/")
+        }
+    }
+    render (){
+        // const {location} = this.props
+        return(
+            <span>hello</span>
+            
+        )
+    }
 }
 
 export default Detail
