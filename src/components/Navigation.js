8cfbf20515d211fa2aac932687a3bd92cf6e01bc
charade6 <charade6@naver.com>
2021-11-03 15:23:15 +0900

    Edit Navigation.js
    link to

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,10 @@
+import { Link } from 'react-router-dom'
+
 function Navigation() {
     return(
         <div>
-            <a href="/">Home</a>
-            <a href="/#/about">About</a>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
         </div>
     )
 }
